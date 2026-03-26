8e0a2be2099774a68ac8cbfd5d8b224dc40ed0b3
seguyr <rachel.seguy@meteo.fr>
2026-03-26 19:23:56 +0100

    Update utils.py

--- functions/utils.py
+++ functions/utils.py
@@ -36,35 +36,31 @@ def load_area_ocean():
 def load_pic_ohc(scale):
     """Load full piControl global OHC time series (scaled to ZJ)."""
     ds = xr.open_dataset(DIR_PIC / "ohc_1D_picontrol.nc")
-
     varname = list(ds.data_vars)[0]
     ohc = ds[varname] / scale
-
     return ohc.assign_coords(time=np.arange(ohc.sizes["time"]))
 
 
 def load_pic_amoc():
     """Load full piControl AMOC time series (Sv)."""
     ds = xr.open_dataset(DIR_PIC / "amoc_picontrol.nc")
-
     amoc = ds["amoc"]
-
     return amoc.assign_coords(year=np.arange(amoc.sizes["year"]))
 
 
-def load_integrated_ohc(filepath, area_oce):
-    """Horizontally integrate OHC field."""
-    ds = xr.open_mfdataset(str(filepath))
-
-    return (ds * area_oce).sum(dim=("x", "y")).__xarray_dataarray_variable__
+def load_integrated_ohc(dataset_type, area_oce):
+    if dataset_type == "hist_tot":
+        filepath = DIR_HIST_TOT / "ohc_2D_hist_tot.nc"
+    elif dataset_type == "hist_3000":
+        filepath = DIR_HIST_3000 / "ohc_2D_hist_3000.nc"
+    ds = xr.open_dataset(filepath)
+    varname = list(ds.data_vars)[0]
+    return (ds[varname] * area_oce).sum(dim=("x", "y"))
 
 
 def load_branching_years():
     """Return branching years for both ensembles."""
-
     hist_starty = xr.open_dataset(HIST_START_YEARS_FILE)["year"]
-
     starty_tot = hist_starty[0:29].values.tolist()
     starty_3000 = hist_starty[29:39].values.tolist()
-
     return starty_tot, starty_3000
