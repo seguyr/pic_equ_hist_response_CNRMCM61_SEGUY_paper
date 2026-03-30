ccb68a805d269c82aa51749c16055641bd5b1aaf
seguyr <rachel.seguy@meteo.fr>
2026-03-30 09:16:04 +0200

    Update run_all.sh

--- scripts/preprocess/run_all.sh
+++ scripts/preprocess/run_all.sh
@@ -17,12 +17,12 @@ echo ""
 echo "Step 1: OHC preprocessing"
 echo "--------------------------------------"
 
-nohup bash ohc/run_all_members.sh &
-nohup bash ohc/04_build_hist_tot.sh &
+bash ohc/run_all_members.sh 
+bash ohc/04_build_hist_tot.sh 
 #bash ohc/05_build_hist_3000.sh 
-nohup bash ohc/06_compute_ohc_picontrol.sh &
-nohup bash ohc/07_build_pic_members.sh &
-nohup bash ohc/08_build_pic_tot.sh &
+bash ohc/06_compute_ohc_picontrol.sh 
+bash ohc/07_build_pic_members.sh 
+bash ohc/08_build_pic_tot.sh 
 #bash ohc/09_build_pic_3000.sh
 
 
@@ -30,13 +30,13 @@ echo ""
 echo "Step 2: AMOC preprocessing"
 echo "--------------------------------------"
 
-nohup bash amoc/11_build_pic_amoc_members.sh &
-nohup bash amoc/12_build_pic_amoc_tot.sh &
+bash amoc/11_build_pic_amoc_members.sh 
+bash amoc/12_build_pic_amoc_tot.sh 
 #bash amoc/13_build_pic_amoc_3000.sh 
-nohup bash amoc/14_build_hist_amoc_members.sh &
-nohup bash amoc/15_build_hist_amoc_tot.sh &
+bash amoc/14_build_hist_amoc_members.sh 
+bash amoc/15_build_hist_amoc_tot.sh 
 #bash amoc/16_build_hist_amoc_3000.sh 
-nohup bash amoc/17_build_picontrol_amoc_timeseries.sh &
+bash amoc/17_build_picontrol_amoc_timeseries.sh 
 
 
 echo ""
