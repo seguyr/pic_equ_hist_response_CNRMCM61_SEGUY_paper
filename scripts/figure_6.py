f2054945075776b90fcc81dd9ee449ae32e4a7ca
seguyr <rachel.seguy@meteo.fr>
2026-03-30 11:39:08 +0200

    Update figure_6.py

--- scripts/figure_6.py
+++ scripts/figure_6.py
@@ -97,8 +97,6 @@ colors[i0_low:i0_high] = [1, 1, 1, 1]
 cmap_custom = mcolors.ListedColormap(colors)
 norm = mcolors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
 
-
-
 # -----------------------------
 # FIGURE 
 # -----------------------------
@@ -115,9 +113,9 @@ gs = fig.add_gridspec(
 axes = [fig.add_subplot(gs[i, 0], projection=proj) for i in range(3)]
 cax  = fig.add_subplot(gs[:, 1])
 
-cf = plot_panel(axes[0], hist_cor_anom_1000, "Hist_dd +1000", "a)")
-plot_panel(axes[1], hist_cor_anom_3000, "Hist_dd +3000", "b)")
-plot_panel(axes[2], diff_hist_cor, "Hist_dd +3000 − Hist_dd +1000", "c)")
+cf = plot_panel(axes[0], hist_gain_1000, "Hist_dd +1000", "a)", CMAP_CUSTOM, NORM)
+plot_panel(axes[1], hist_gain_3000, "Hist_dd +3000", "b)", CMAP_CUSTOM, NORM)
+plot_panel(axes[2], diff_hist_gain, "Hist_dd +3000 − Hist_dd +1000", "c)", CMAP_CUSTOM, NORM)
 
 cbar = fig.colorbar(
     cf, cax=cax,
